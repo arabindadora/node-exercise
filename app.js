ac613558b345f114d824151c8283b10470d75727
Arvind Kumar <arvee4u@yahoo.in>
2016-08-16 03:21:18 +0530

    [Refactor] 1. Move to ES6. Yay! 2. Use vanilla for loop vs. _.each in getCharacterData()

--- app.js
+++ app.js
@@ -2,24 +2,25 @@
  * Arvind Kumar
  * arvind.kumar@msn.com
  */
+"use strict";
 
-var request = require('request-promise');
-var express = require('express');
-var app     = express();
-var _       = require('lodash');
- 
+const request = require('request-promise');
+const express = require('express');
+const app     = express();
+const _       = require('lodash');
+
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/character/:name', (req, res) => {
-    var name    = req.params.name;
-    var baseURL = 'http://swapi.co/api/people/';
-    
-    getCharacterData(baseURL, name)
+    let name  = req.params.name;
+    const url = 'http://swapi.co/api/people/';
+
+    getCharacterData(url, name)
     .then((response) => {
         res.render('character', response);
     })
@@ -29,26 +30,30 @@ app.get('/character/:name', (req, res) => {
 
     function getCharacterData(url, name) {
         return new Promise((resolve, reject) => {
-            request(url)
-            .then((body) => {
+            request(url).then((body) => {
                 if(!body) return reject('Couldn\'t fetch data!');
 
                 name = name.toLowerCase();
                 body = JSON.parse(body);
-                var people = body.results;
+                const people = body.results;
 
-                _.each(people, (character) => {
-                    var charName = character.name.toLowerCase();
-                    var isMatch  = charName === name || charName.split(' ').indexOf(name) > -1;
+                // use vanilla forloop as _.each still runs even after returning
+                for(let i=0, l=people.length; i < l; i++) {
+                    const character = people[i];
+                    const charName  = character.name.toLowerCase(); // lowerCase as the request data could be in any case
+                    // match full name or first/middle/last name as request data could contain any of these
+                    const isMatch   = charName === name || charName.split(' ').indexOf(name) > -1;
+
+                    // if said character is found, return desired data
                     if(isMatch) {
                         return resolve(_.pick(character, ['name', 'gender', 'height', 'mass', 'birth_year']));
                     }
-                });
+                }
 
                 if (!body.next || body.next === null) {
                     return reject(`No match found for ${name}!`);
                 }
-                
+
                 getCharacterData(body.next, name)
                 .then((response) => {
                     resolve(response);
@@ -61,20 +66,19 @@ app.get('/character/:name', (req, res) => {
     }
 });
 
-app.get('/characters', function (req, res) {
-    var base = 'http://swapi.co/api/people/';
-    var urls = [base, base + '?page=2', base + '?page=3', base + '?page=4', base + '?page=5'];
-    
-    var all = urls.map(function (url) {
-        return new Promise(function (resolve, reject) {
-            request(url)
-                .then(function (body) {
-                    resolve(JSON.parse(body).results);
-            });            
+app.get('/characters', (req, res) => {
+    const base = 'http://swapi.co/api/people/';
+    const urls = [base, base + '?page=2', base + '?page=3', base + '?page=4', base + '?page=5'];
+
+    const all = urls.map((url) => {
+        return new Promise((resolve, reject) => {
+            request(url).then((body) => {
+                resolve(JSON.parse(body).results);
+            });
         });
     });
 
-    Promise.all(all).then(function (response) {
+    Promise.all(all).then((response) => {
         response = _.flatten(response);
 
         if('sort' in req.query) {
@@ -85,29 +89,30 @@ app.get('/characters', function (req, res) {
     })
 });
 
-app.get('/planetresidents', function (req, res) {
+app.get('/planetresidents', (req, res) => {
     function getData() {
-        return new Promise(function(resolve, reject) {
-            request('http://swapi.co/api/planets')
-            .then(function (body) {
-                var allPlanets = JSON.parse(body).results;
-                _.each(allPlanets, function (planet) {
-                    var planet = _.pick(planet, ['name', 'residents']);
-                    var keyval = {};
+        return new Promise((resolve, reject) => {
+            const url = 'http://swapi.co/api/planets';
 
-                    var all = planet.residents.map(function (resident) {
-                        return new Promise(function (resolve, reject) {
-                            request(resident)
-                            .then(function (body) {
+            request(url).then((body) => {
+                const allPlanets = JSON.parse(body).results;
+
+                _.each(allPlanets, (planet) => {
+                    let keyval = {};
+                    planet = _.pick(planet, ['name', 'residents']);
+
+                    const all = planet.residents.map((resident) => {
+                        return new Promise((resolve, reject) => {
+                            request(resident).then((body) => {
                                 resolve(JSON.parse(body).name);
                             });
                         })
                     });
 
-                    Promise.all(all).then(function (residents) {
-                        var response = {};
+                    Promise.all(all).then((residents) => {
+                        let response = {};
                         keyval[planet.name] = residents;
-                        _.assign(response, keyval); 
+                        _.assign(response, keyval);
                         resolve(response);
                     });
                 });
@@ -115,12 +120,12 @@ app.get('/planetresidents', function (req, res) {
         });
     };
 
-    getData().then(function (response) {
+    getData().then((response) => {
         res.send(response);
     });
 })
 
-var port = process.env.port || 3000;
-app.listen(port, function () {
-  console.log('app running at - http://localhost:' +port);
+const port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log('app running at - http://localhost:' +port);
 });
